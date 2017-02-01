b0ed798c0008fafd40643335e80d06f30d481a12
L3W20 <lw03232@gmail.com>
2017-01-31 23:22:05 -0600

    switched to hardcoded weights

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ import sys
 
 infile = "input.txt"
 lexfile = "lexicon.csv"
-lexfiles = []
-print sys.argv[2:]
-if len(sys.argv)>1: infile = sys.argv[1]
-if len(sys.argv)>2: lexfile = sys.argv[2]
-if len(sys.argv)>2:
-    for arg in range(len(sys.argv[2:])): lexfiles.append(sys.argv[arg+2])
-print lexfiles
+infiles = []
+if len(sys.argv)>1:
+    for arg in range(len(sys.argv[1:])): infiles.append(sys.argv[arg+1])
+print infiles
 f = open(infile,"r")
 s = f.read().lower()
 
@@ -20,11 +17,9 @@ s = f.read().lower()
 wordsplit = s.translate(string.maketrans("",""), string.punctuation)
 wordsplit = wordsplit.split()
 
-wordlist = []
-#polarity = []
-
+"""#wordlist = []
 #Adds the first value of each row of the csv to a list
-lexicon = lexfiles[0]
+*lexicon = lexfiles[0]
 for lex in range(len(lexfiles)):
     lexicon = lexfiles[lex]
     with open(lexicon, 'rb') as f:
@@ -32,21 +27,80 @@ for lex in range(len(lexfiles)):
         for row in reader:
             if not row[0] in wordlist:
                 wordlist.append(row[0])
-                #polarity.append(row[1])
+                #polarity.append(row[1])"""
 
-#Sorts the csv list by descending length
-#wordlist.sort(key=lambda x:len(x[0]), reverse=True)
 
+ ### TESTING FOR NEGEMO WORDS ###
+wordlist = []
+polarity = []
 poswords = 0.0
 negwords = 0.0
+
+with open("lexicon.csv", 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if not row[0] in wordlist:
+            wordlist.append(row[0])
+            polarity.append(row[1])
+
+print "TESTING NEGEMO"
 for i in range (len(wordsplit)):
     if wordsplit[i] in wordlist:
-        print "Found: "+wordsplit[i]+"!"
-        #print polarity[wordlist.index(wordsplit[i])]
-        #if polarity[wordlist.index(wordsplit[i])] == "positive":
-            #poswords += 1
-        #elif polarity[wordlist.index(wordsplit[i])] == "negative":
-            #negwords += 1
+        if polarity[wordlist.index(wordsplit[i])] == "positive":
+            poswords+=1
+        else:
+            negwords+=1
 
-print "POSITIVE WORDS: "+str(poswords)
-print "NEGATIVE WORDS: "+str(negwords)
+negtotal = negwords/len(wordsplit)
+negtotal = negtotal*(-0.237)
+print negtotal
+
+ ### TESTING FOR EXCLUSIVE WORDS ###
+wordlist = []
+extotal = 0;
+
+with open("exclusives.csv", 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if not row[0] in wordlist:
+            wordlist.append(row[0])
+
+print "TESTING EXCLUSIVE"
+for i in range (len(wordsplit)):
+    if wordsplit[i] in wordlist:
+        extotal+=1
+
+extotal = extotal/len(wordsplit)
+extotal = extotal*(0.54)
+print extotal
+
+ ### TESTING FOR FIRST PERSON ###
+wordlist = []
+fptotal = 0;
+
+with open("firstperson.csv", 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if not row[0] in wordlist:
+            wordlist.append(row[0])
+
+print "TESTING FIRST PERSON"
+print wordlist
+for i in range (len(wordsplit)):
+    if wordsplit[i] in wordlist:
+        fptotal+=1
+
+fptotal = fptotal/len(wordsplit)
+fptotal = fptotal*(0.36)
+print fptotal
+
+ ### RUNNING FINAL CALCULATIONS ###
+total = fptotal + extotal + negtotal
+print total
+
+
+
+
+"""for i in range (len(wordsplit)):
+    if wordsplit[i] in wordlist:
+        print "Found: "+wordsplit[i]+"!" """
